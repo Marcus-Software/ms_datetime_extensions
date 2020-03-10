1ffb149794d2a6b0b8faae66361189caa43c1e3d
Marcus Duarte <marcusedu7@gmail.com>
2020-03-10 11:43:33 -0300

    dateTime fromInternet implemented

---
+++ test/src/worldtimeapi_test.dart
@@ -0,0 +1,11 @@
+import 'package:ms_datetime_extensions/ms_datetime_extensions.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  test('Must return a valid datetime', () async {
+    var nowFromInternet =
+        await fromInternet(timezone: Timezones.americaSaoPaulo);
+    expect(nowFromInternet, isNotNull);
+    expect(nowFromInternet.timeZoneOffset, Duration(hours: -3));
+  });
+}
